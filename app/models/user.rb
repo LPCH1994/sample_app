3f7e0ec3c62cc2d218382f2607cc5cb8385f14b7
LPCH1994 <hsblh32@gmail.com>
2016-02-02 14:49:23 +0700

    Finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 	format: {with: VALID_EMAIL_REGEX },
 	uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password,presence: true,length: {minimum: 8}
-	
+	validates :password,presence: true,length: {minimum: 4}
+
 end

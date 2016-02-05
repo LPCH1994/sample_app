230e8b3408741032dfd5ce2a1cd627cf35a4799a
LPCH1994 <hsblh32@gmail.com>
2016-02-05 08:49:21 +0700

    Finish user edit,update,index,destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 	format: {with: VALID_EMAIL_REGEX },
 	uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password,presence: true,length: {minimum: 4}
+	validates :password,presence: true,length: {minimum: 4},allow_nil: true
   #return hash digest
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :

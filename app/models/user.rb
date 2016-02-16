eb0cbd3e6285355492935b4e7b4b4bf50012df72
LPCH1994 <hsblh32@gmail.com>
2016-02-16 16:37:10 +0700

    fix syntax2

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
 
   #active an account
 
-  def active
+  def activate
     update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
